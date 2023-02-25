511db1e6c50fcbf913aa83cbe39a31522edb75a4
MatthieuMarchand <matthieu.marchand@mmibordeaux.com>
2023-02-25 20:29:01 +0100

    test

--- JS/labJS/pokemonCard.js
+++ JS/labJS/pokemonCard.js
@@ -13,8 +13,8 @@ function handleMouseMove(e) {
     let glowY = y / elRect.height * 100;
     el.children[0].style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`;
     el.children[1].style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`;
-    el.children[1].style.background = `radial-gradient(circle at ${glowX}% ${glowY}%, salmon, transparent)`;
-    this.children[1].style.opacity = "0.3";
+    el.children[1].style.background = `radial-gradient(circle at ${glowX}% ${glowY}%, green, transparent)`;
+    this.children[1].style.opacity = "1";
 }
 
 function resetCardTransform() {
